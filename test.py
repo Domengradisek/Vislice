a8da2141fabfc9fb79ac1bb83ae7836a8f9e1370
Domen <domko.grad@gmail.com>
2020-05-19 19:46:07 +0200

    Popravil datoteko test

--- test.py
+++ test.py
@@ -1,26 +1,25 @@
 def je_prastevilo(n):
-    i = 2
     if n <= 1:
         return False
     elif n == 2:
         return True
     else:
+        i = 2
         while i < n / 2 + 1:
             if n % i != 0:
-                return False
-            else:
                 i += 1
+            else:
+                return False
         return True
 
-def prastevila_do(n):
-    seznam =[]
-    i = 3
-    while i <= n:
-        if je_prastevilo(i):
-            seznam.append(i)
-            i += 1
+def prastevila_do_(n):
+    seznam = []
+    while n > 0:
+        if je_prastevilo(n):
+            seznam.append(n)
+            n -= 1
         else:
-            i += 1
-    return [2] + seznam
+            n -= 1
+    return seznam
 
-print(prastevila_do(200))
+print(prastevila_do_(200))
